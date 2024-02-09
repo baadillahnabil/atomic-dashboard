c25532f2085c3ee6124152697314f5762fd3abfa
baadillahnabil <baadillahnabil@gmail.com>
2024-02-09 17:02:05 +0800

    feat: move content to pages

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -1,50 +1,28 @@
-import type { FC } from "react";
+import type { FC, PropsWithChildren, ReactNode } from "react";
 import clsx from "clsx";
-
-import Button from "atoms/Button";
-import Icon from "atoms/Icons";
-import HomeIcon from "@/src/components/atoms/Icons/Home";
-import BellIcon from "@/src/components/atoms/Icons/Bell";
-import ClockIcon from "@/src/components/atoms/Icons/Clock";
-import PeopleIcon from "@/src/components/atoms/Icons/People";
-import WalletIcon from "@/src/components/atoms/Icons/Wallet";
-import SettingsIcon from "@/src/components/atoms/Icons/Settings";
-import ButtonGroup from "molecules/ButtonGroup";
 
 import styles from "./style.module.scss";
 
 interface SidebarProps {
   className?: string;
-  logo?: string;
+  logo?: ReactNode;
+  footer?: ReactNode;
 }
 
-const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
+const Sidebar: FC<PropsWithChildren<SidebarProps>> = ({
+  className,
+  children,
+  logo,
+  footer,
+  ...props
+}) => {
   const classes = clsx(styles.sidebar, className);
 
   return (
     <div className={classes} {...props}>
-      <p>logo</p>
-      <ButtonGroup border="circle" position="vertical" verticalGap="medium">
-        <Button variant="icon" border="circle" color="primary">
-          <Icon icon={HomeIcon} />
-        </Button>
-        <Button variant="icon" border="circle" color="none">
-          <Icon icon={BellIcon} />
-        </Button>
-        <Button variant="icon" border="circle" color="none">
-          <Icon icon={ClockIcon} />
-        </Button>
-        <Button variant="icon" border="circle" color="none">
-          <Icon icon={PeopleIcon} />
-        </Button>
-        <Button variant="icon" border="circle" color="none">
-          <Icon icon={WalletIcon} />
-        </Button>
-        <Button variant="icon" border="circle" color="none">
-          <Icon icon={SettingsIcon} />
-        </Button>
-      </ButtonGroup>
-      <p>logout</p>
+      <div className={styles.logo}>{logo}</div>
+      <div className={styles.content}>{children}</div>
+      <div className={styles.footer}>{footer}</div>
     </div>
   );
 };
--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,7 +1,26 @@
+import Avatar from "atoms/Avatar";
+import Button from "atoms/Button";
+import Icon from "atoms/Icons";
+
+import HomeIcon from "atoms/Icons/Home";
+import BellIcon from "atoms/Icons/Bell";
+import ClockIcon from "atoms/Icons/Clock";
+import PeopleIcon from "atoms/Icons/People";
+import WalletIcon from "atoms/Icons/Wallet";
+import SettingsIcon from "atoms/Icons/Settings";
+import ChatIcon from "atoms/Icons/Chat";
+import LogoutIcon from "atoms/Icons/Logout";
+
+import ButtonGroup from "molecules/ButtonGroup";
+
 import Header from "organisms/Header";
 import Sidebar from "organisms/Sidebar";
+
 import DashboardMainTemplate from "templates/Dashboard/Main";
 import DashboardHomeTemplate from "templates/Dashboard/Home";
+
+import Logo from "assets/images/logo.png";
+import User from "assets/images/user.png";
 
 import styles from "./style.module.scss";
 
@@ -9,24 +28,64 @@ function Dashboard() {
   return (
     <div className={styles.dashboard}>
       <DashboardMainTemplate
-        sidebar={<Sidebar />}
-        content={
-          <DashboardHomeTemplate
-            body={
-              <Header
-                title={{
-                  text: "Hello, Jhon .D",
-                  titleProps: {
-                    subtitle: "View and control your finances here",
-                    subtitleProps: { variant: "h5", size: "subtitle" },
-                    gap: "medium",
-                  },
-                }}
-              />
-            }
-          />
+        sidebar={
+          <Sidebar logo={<Avatar src={Logo} alt="logo" size="medium" />}>
+            <Button variant="icon" border="circle" color="secondary">
+              <Icon icon={ChatIcon} />
+            </Button>
+            <ButtonGroup
+              border="circle"
+              position="vertical"
+              verticalGap="medium"
+            >
+              <Button variant="icon" border="circle" color="primary">
+                <Icon icon={HomeIcon} />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={BellIcon} />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={ClockIcon} />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={PeopleIcon} />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={WalletIcon} />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={SettingsIcon} />
+              </Button>
+            </ButtonGroup>
+            <ButtonGroup border="circle" position="vertical" verticalGap="none">
+              <Button variant="icon" border="circle" color="none" compact>
+                <Avatar src={User} alt="user_avatar" size="medium" />
+              </Button>
+              <Button variant="icon" border="circle" color="none">
+                <Icon icon={LogoutIcon} size={20} />
+              </Button>
+            </ButtonGroup>
+          </Sidebar>
         }
-      />
+      >
+        <DashboardHomeTemplate sidebar={<p>this is right sidebar</p>}>
+          <Header
+            title={{
+              text: "Hello, Jhon .D",
+              titleProps: {
+                subtitle: "View and control your finances here",
+                subtitleProps: { variant: "h5", size: "subtitle" },
+                gap: "medium",
+              },
+            }}
+          />
+        </DashboardHomeTemplate>
+
+        {/* 
+          There will be more templates for other dashboard pages like
+          notifications, settings, etc., based on the sidebar navigation. 
+        */}
+      </DashboardMainTemplate>
     </div>
   );
 }
